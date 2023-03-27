d7f954904fadd6f84085cbcb2a80a706c3980a07
José Francisco Dos Santos Neto <jf9718830@gmail.com>
2023-03-27 07:57:03 -0300

    Terminado o Program.cs

--- Pokedex/Services/PokeService.cs
+++ Pokedex/Services/PokeService.cs
@@ -6,4 +6,30 @@ namespace Pokedex.Services;
 public class PokeService : IPokeService
 {
     private readonly IHttpContextAcessor _session;
+    private readonly string pokemonFile = @"Data\pokemons.json";
+    private readonly string tiposFile = @"Data\tipos.json";
+
+    public PokeService(IHttpContextAccessor session)
+    {
+        _session = session;
+        PopularSessao();
+    }
+
+    public List<Pokemon> GetPokemons()
+    {
+        PopularSessao();
+        var pokemons = JsonSerializer.Deserialize<List<Pokemon>>
+            (_session.HttpContext.Session.getString("Pokemons"));
+        return pokemons;
+    }
+
+    public List<Tipo> GetTipos()
+    {
+        PopularSessao();
+        var pokemons = JsonSerializer.Deserialize<List<Tipo>>
+            (_session.HttpContext.Session.getString("Tipos"));
+        return tipos;
+    }
+
+    
 }
